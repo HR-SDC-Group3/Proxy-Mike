e27990834fda5b1608b20d28ac6b974bed8bfce3
Michael <michael99940@gmail.com>
2019-01-08 11:58:04 -0800

    Add files via upload

---
+++ client/src/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
+import $ from 'jquery';
+const axios = require('axios');
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  // render() {
+  //   return (
+  //     <div>
+  //       Hello World 2
+  //     </div>
+  //   );
+  // }
+}
+
+// if (typeof window !== 'undefined') {
+//   ReactDOM.render(<App />, document.getElementById('app'));
+// }
