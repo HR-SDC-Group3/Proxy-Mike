2056d441e72a69a2307406751df6d6729fa23b50
michael99940 <michael99940@gmail.com>
2019-01-23 19:03:06 -0800

    ec2 sdc deploy

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
+require('newrelic');
 const express = require('express');
 const bodyParser = require('body-parser');
-const proxy = require('http-proxy-middleware')
+const proxy = require('http-proxy-middleware');
 
 const app = express();
 
 app.use('/restaurants/:id/', express.static(`${__dirname}/../client/dist/`));
 app.use(bodyParser());
 
-app.use('/api/reservations/restaurantID=:restaurantID&date=:date', proxy({ target: 'http://54.183.92.72:3002', changeOrigin: true }));
-app.use('/api/reservations/', proxy({ target: 'http://54.183.92.72:3002', changeOrigin: true }));
+app.use('/api/reservations/restaurantID=:restaurantID&date=:date', proxy({ target: 'http://18.144.29.119:3002', changeOrigin: true }));
+app.use('/api/reservations/', proxy({ target: 'http://18.144.29.119:3002', changeOrigin: true }));
 app.use('/api/restaurants/:id/reviews', proxy({ target: 'http://54.183.92.72:3004', changeOrigin: true }));
 app.use('/api/restaurants/:id/menu', proxy({ target: 'http://ec2-13-57-40-25.us-west-1.compute.amazonaws.com', changeOrigin: true }));
 app.use('/api/restaurants/:id/photos', proxy({ target: 'http://ec2-18-222-226-95.us-east-2.compute.amazonaws.com', changeOrigin: true }));
